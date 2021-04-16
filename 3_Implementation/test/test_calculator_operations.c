73bbfbce36892ae0f65bb3b984c5b2753764b682
shirisha9010 <82135750+shirisha9010@users.noreply.github.com>
2021-04-16 10:52:17 +0530

    Update test_calculator_operations.c

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -1,18 +1,34 @@
 #include "unity.h"
-#include <gpa.h>
+#include <calculator_operations.h>
+#define PROJECT_NAME    "CALCULATOR"
 
-/* Modify these two lines according to the project */
-#include <gpa.h>
-#define PROJECT_NAME    "GPA/CGPA calculator"
-
-/* Prototypes for all the test functions */
-void semester();
-void cgpa_calculator();
-
+operand test={3,1},test1={-3,-1};
+int v1=6,v2=2,v3=4;
+int v4=-6,v5=-2,v6=-4;
+int d1=2,d2=0;
 /* Required by the unity test framework */
 void setUp(){}
 /* Required by the unity test framework */
 void tearDown(){}
+
+/* Prototypes for all the test functions */
+void test_add(void);
+void test_subtract(void);
+void test_multiply(void);
+void test_divide(void);
+void test_divide_by0(void);
+void test_s_root(void);
+void test_fact(void);
+void test_fact_negative(void);
+void test_fact_by0(void);
+void test_Rec(void);
+void test_Sqr(void);
+void test_Para(void);
+void test_Trai(void);
+
+
+
+
 
 /* Start of the application test */
 int main()
@@ -21,25 +37,79 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(semester);
-  RUN_TEST(cgpa_calculator);
+  RUN_TEST(test_add);
+  RUN_TEST(test_subtract);
+  RUN_TEST(test_multiply);
+  RUN_TEST(test_divide);
+  RUN_TEST(test_s_root);
+  RUN_TEST(test_fact);
+  RUN_TEST(test_Rec);
+RUN_TEST(test_fact_negative);
+RUN_TEST(test_fact_by0);
+RUN_TEST(test_divide_by0);
+RUN_TEST(test_Sqr);
+RUN_TEST(test_Para);
+RUN_TEST(test_Trai);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void semester(void) {
-  TEST_ASSERT_EQUAL(9.0, add(, 20));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(15000, add(7500, 7500));
+void test_add(void) {
+  TEST_ASSERT_EQUAL(4, addition(&test));
+  TEST_ASSERT_EQUAL(-4, addition(&test1));
 }
 
-void cgpa_calculator(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(100, subtract(1000, 900));
+void test_subtract(void) {
+  TEST_ASSERT_EQUAL(2,subtraction(&test));
+  TEST_ASSERT_EQUAL(-2, subtraction(&test1));
 }
 
+void test_multiply(void) {
+  TEST_ASSERT_EQUAL(12, multiplication(&v1,&v2));
+  TEST_ASSERT_EQUAL(12, multiplication(&v4,&v5));
+}
+
+void test_divide(void) {
+  TEST_ASSERT_EQUAL(3, division(&v1,&v2));
+  TEST_ASSERT_EQUAL(3, division(&v4,&v5));
+}
+void test_divide_by0(void){
+  TEST_ASSERT_EQUAL(-1, division(&d1,&d2));
+}
+
+void test_s_root(void) {
+  TEST_ASSERT_EQUAL(2, squareroot(&v3));
+}
+
+void test_fact(void) {
+  TEST_ASSERT_EQUAL(24, factorial(&v3));
+}
+
+void test_fact_negative(void) {
+  TEST_ASSERT_EQUAL(-1, factorial(&v6));
+}
+
+void test_fact_by0(void) {
+  TEST_ASSERT_EQUAL(1, factorial(&d2));
+}
+
+void test_Rec(void) {
+  TEST_ASSERT_EQUAL(12, Area_of_Rectangle(&v1,&v2));
+  TEST_ASSERT_EQUAL(12, Area_of_Rectangle(&v4,&v5));
+}
+
+void test_Sqr(void) {
+  TEST_ASSERT_EQUAL(16, Area_of_square(&v3));
+  TEST_ASSERT_EQUAL(16, Area_of_square(&v6));
+
+}
+
+void test_Para(void) {
+  TEST_ASSERT_EQUAL(24, Area_of_Parallelogram(&v1,&v3));
+ TEST_ASSERT_EQUAL(24, Area_of_Parallelogram(&v4,&v6));
+}
+void test_Trai(void) {
+  TEST_ASSERT_EQUAL(36, Area_of_Trai(&v1,&v2));
+  TEST_ASSERT_EQUAL(36, Area_of_Trai(&v4,&v5));
