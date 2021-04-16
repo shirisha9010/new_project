79c060ceb17eba60385bf030077f871aa84918ef
shirisha9010 <82135750+shirisha9010@users.noreply.github.com>
2021-04-16 10:51:06 +0530

    Update calculator_operations.c

--- 3_Implementation/src/calculator_operations.c
+++ 3_Implementation/src/calculator_operations.c
@@ -1,54 +1,77 @@
-#include<stdio.h>
-int cgpa_calculator()
-{
-int i=0,semesters,credits[100],total_credits=0;
-float gpa[100], cgpa,total=0;
-printf("Enter the number of semesters for which you want calculate the CGPA:"); 
-scanf("%d",&semesters);
-printf("\n");
-for(i=1;i<=semesters;i++)
-{
-printf("Enter the GPA of semester:");
-scanf("%f",&gpa[i]);
-printf("Enter the total credits in semester:");
-scanf("%d",&credits[i]);
-printf("\n");
+#include "calculator_operations.h"
+#include "stdio.h"
+/**
+ * @brief   FUNCTION DECLARATION OF ALL THE FUNCTIONS CALLED
+ * 
+ * @param find 
+ * @return int 
+ */
+int addition(operand *find){
+   int final=find->ope1 + find->ope2;                     // addition of operands
+   printf(" Addition of %d + %d = %d\n",find->ope1,find->ope2,final);
+   return final;
 }
-for(i=1;i<=semesters;i++)
-{
-total=total+(gpa[i]*10*credits[i]);
-total_credits=total_credits+credits[i];
-}
-cgpa=(total/total_credits);
-printf("The cumulative GPA of all semesters is: %f",cgpa);
-printf("\n");
+int subtraction(operand  *find){
+   int final=(find->ope1)-(find->ope2);                     // subtraction of operands
+   printf("subtraction of %d - %d = %d\n",find->ope1,find->ope2,final);
+   return final;
 }
 
+int multiplication(int *operand1,int *operand2){
+   int final= (*operand1)*(*operand2);                         // multiplication of operands
+    printf(" multiplication = %d\n",final);
+    return final;
+}
 
-int semester()
-{
+int division(int *operand1,int *operand2){
+    if(*operand2==0){
+        printf("divisor can;t be zero");                 // division when any one of the operand is 0
+        return -1;
+    }
 
-	int sum=0,total_subjects,credits=0;
-	float GPA;
-	int i,arr[100],arr1[100];
-	printf("Enter the number of subjects in semester:");
-	scanf("%d",&total_subjects);
-	printf("\n");
-	for(i=0;i<total_subjects;i++)
-	{
-	printf("Enter the gradepoint obtained:");
-	scanf("%d",&arr[i]);
-	printf("Enter the credits:");
-	scanf("%d",&arr1[i]);
-	printf("\n");
-	}
-	printf("\n\n");
-	for(i=0;i<total_subjects;i++)
-	{
-	sum = sum+(arr[i]*arr1[i]);
-	credits=credits+arr1[i];
-	}
-	GPA=(sum/credits);
-	printf("Your GPA is : %f",GPA);
-	printf("\n");
+   int final= (*operand1)/(*operand2);                  
+    printf(" quotient = %d\n",final);
+    printf(" remainder = %d\n",(*operand1)%(*operand2));     // division of 2 operands
+    return final;
+}
+int squareroot(int *operand3){
+   int final= sqrt(*operand3);               
+    printf(" squareroot %d = %d\n",*operand3,final);
+    return final;
+}
+int factorial(int *operand3){
+  if(*operand3 < 0)
+    {printf("factorial for negative numbers not possible\n");          // factorial of the operand
+      return -1;}
+  if(*operand3 == 0)
+   { return 1;}
+   int fact=1;
+    for (int i = 1; i <= *operand3;i++) {
+            fact *= i;
+        }
+ printf("%d! = %d\n",*operand3,fact);
+ return fact;
+}
+
+int Area_of_Rectangle(int *operand1,int *operand2){
+   float final= (*operand1)*(*operand2);               
+    printf(" Area_of_Rectangle = %.3f\n",final);                             // Area_of_Rectangle of operand
+    return final;
+}
+
+int Area_of_Square(int *operand3){
+   float final= (*operand3);               
+    printf("Area_of_Square = %.3f\n",final);                                // Area_of_Square of operand
+    return final;
+}
+
+int Area_of_Parallelogram(int *operand1,int *operand2){
+   int final= (*operand1)*(*operand2);               
+    printf(" Area_of_parallelogram = %d\n",final);                           //  Area_of_Parallelogram operand
+    return final;
+}
+int Area_of_Triangle(int *operand1,int *operand2){
+   int final= (0.5)*+(*operand1)*(*operand2);               
+    printf(" Area_of_Triangle = %d\n",final);                                // Area_of_Triangle of operand
+    return final;
 }
